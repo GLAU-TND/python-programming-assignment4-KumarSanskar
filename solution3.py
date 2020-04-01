95ff1f0a67c48bab0fbcc0e93f8ce145b1dede87
Kumar Sanskar <61062977+KumarSanskar@users.noreply.github.com>
2020-04-01 09:43:11 +0530

    Solution 3
    solution 3

--- solution3.py
+++ solution3.py
@@ -1,5 +1,5 @@
-a = eval(input())  # takes input from user
-ls = eval(input())  # user will input a list with certain elements
+ls = eval(input("enter a list "))  # user enter a list with elements
+a = eval(input("enter the query string "))  # user enters a query-string to find element starting with it as a prefix in list
 b = []
 for i in ls:
     if i.startswith(a)==True:
